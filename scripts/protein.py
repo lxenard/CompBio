19bd4944309c4f01ee3501bb57e7f3c1fb1a4caf
Mini-Miette <laura.xenard@protonmail.com>
2021-09-05 18:21:03 +0200

    Creation of Point and Residue classes.

---
+++ scripts/protein.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  5 16:55:30 2021
+
+@author: Laura Xénard
+"""
+
+class Point:
+
+    def __init__(self, x=0, y=0, z=0):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __repr__(self):
+        return f"[{self.x}, {self.y}, {self.z}]"
+
+    def __add__(self, p):
+        x = self.x + p.x
+        y = self.y + p.y
+        z = self.z + p.z
+        return Point(x, y, z)
+
+    def __sub__(self, p):
+        x = self.x - p.x
+        y = self.y - p.y
+        z = self.z - p.z
+        return Point(x, y, z)
+
+
+class Residue:
+
+    def __init__(self, aa='', p=Point(), asa=0):
+        self.aa = aa
+        self.coord = p
+        self.asa = asa
+
+    def __repr__(self):
+        return f"({self.aa}, coord: {self.coord}, asa: {self.asa})"
+
+    def is_hydrophobic(self):
+        """
+        Determine if the residue is hydrophobic or not.
+
+        Raises
+        ------
+        ValueError
+            DESCRIPTION.
+
+        Returns
+        -------
+        bool
+            True if the residue is hydrophobic, False otherwise.
+
+        """
+        if self.aa == '':
+            raise ValueError
+
+        # F, G, I, L, M, V, W and Y
+        hydrophobic = ('PHE', 'GLY', 'ILE', 'LEU', 'MET', 'VAL', 'TRP', 'TYR')
+        if self.aa in hydrophobic:
+            return True
+        else:
+            return False
