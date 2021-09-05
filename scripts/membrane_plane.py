4785d63b035a722c6ca1b240ea54d4b852e4ee57
Mini-Miette <laura.xenard@protonmail.com>
2021-09-05 21:08:12 +0200

    Selection of exposed residues, computing the barycenter

--- scripts/membrane_plane.py
+++ scripts/membrane_plane.py
@@ -5,6 +5,7 @@ Created on Sun Sep  5 16:56:22 2021
 @author: Laura Xénard
 """
 
+import argparse
 from pathlib import Path
 import time
 
@@ -14,14 +15,36 @@ from Bio.PDB.DSSP import DSSP
 import protein as ptn
 
 
+def barycenter(residues_list):
+    """
+    Compute the barycenter of a list of residues.
+
+    Parameters
+    ----------
+    residues_list : list(ptn.Residues)
+        The residues from which to find the barycenter.
+
+    Returns
+    -------
+    x_bary, y_bary, z_bary : (float, float, float)
+        Respectively x, y and z coordinates of the residues barycenter.
+
+    """
+    x_bary = sum([r.coord.x for r in residues_list]) / len(residues_list)
+    y_bary = sum([r.coord.y for r in residues_list]) / len(residues_list)
+    z_bary = sum([r.coord.z for r in residues_list]) / len(residues_list)
+    return x_bary, y_bary, z_bary
+
+
 if __name__ == '__main__':
 
     start = time.time()
 
     # TODO: Argument à sortir par argparse
-    pdb_path = "G:/RAID/Fac/M2_BI/PGP/CompBio/data/2n90.pdb"
+    pdb_path = 'G:/RAID/Fac/M2_BI/PGP/CompBio/data/2n90.pdb'
     model = 0
     chain = 'A'
+    IS_EXPOSED_THRESHOLD = 0.3
 
     # Ouverture et parsing du fichier PDB
     p = PDBParser()
@@ -31,13 +54,25 @@ if __name__ == '__main__':
 
 
 
-    # Création des résidus
-    residues = []
-    for i_res, res in enumerate(structure[model][chain]):
 
+    # Selection of the exposed residues.
+    # For better results, the burrowed residues are not taken into account
+    # during the membrane detection.
+    # TODO: à vérifier en comparant les résultats avec le traitement de tous
+    # les résidus
+    residues = []  # Store the exposed residues.
+    for i_res, res in enumerate(structure[model][chain]):
+        # For simplification, the position of a residue is defined as the
+        # position of its Cα.
         pt = ptn.Point(*res['CA'].coord)
-        tmp = ptn.Residue(res.resname, pt, dssp[(chain, i_res+1)][3])
-        residues.append(tmp)
+        asa = dssp[(chain, i_res+1)][3]  # Accessible surface area.
+        tmp = ptn.Residue(res.id[1], res.resname, pt, asa)
+        if tmp.is_exposed(IS_EXPOSED_THRESHOLD):
+            residues.append(tmp)
+
+    print(barycenter(residues))
+
+
 
 
 
@@ -57,4 +92,4 @@ if __name__ == '__main__':
 
 
     end = time.time() - start
-    #print('\nDONE in {:.0f} min {:.2f} s.'.format(end // 60, end % 60))
+    print('\nDONE in {:.0f} min {:.2f} s.'.format(end // 60, end % 60))
